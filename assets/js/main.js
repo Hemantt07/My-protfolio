8e18942fb29cb48bd700eeec9844131bd96b85a9
Hemant Dhiman <reshavdhiman67@gmail.com>
2023-06-05 08:18:54 +0530

    back to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,20 @@ $(document).ready(function(){
         $('header .top-header nav .nav-container').slideToggle();
     });
 
+    // Back to top
+
+    $('#back-to-top').hide();
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100 ) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    
+    $("#back-to-top").click(function() {
+        $("html, body").animate({scrollTop: 0}, 10);
+     });
+
 });
